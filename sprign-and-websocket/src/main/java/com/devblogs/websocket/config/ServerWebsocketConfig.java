decf9701e291ef54e1033578ab22872e2da26272
zheka <zheka0783@gmail.com>
2017-01-25 18:50:32 +0200

    Refactoring

--- sprign-and-websocket/src/main/java/com/devblogs/websocket/config/ServerWebsocketConfig.java
+++ sprign-and-websocket/src/main/java/com/devblogs/websocket/config/ServerWebsocketConfig.java
@@ -2,22 +2,22 @@ package com.devblogs.websocket.config;
 
 import java.util.HashSet;
 import java.util.Set;
-
 import javax.websocket.Endpoint;
 import javax.websocket.server.ServerApplicationConfig;
 import javax.websocket.server.ServerEndpointConfig;
-
 import com.devblogs.websocket.AnotherWebSocketEndpoint;
 import com.devblogs.websocket.WebSocketEndpoint;
 
 public class ServerWebsocketConfig implements ServerApplicationConfig {
     public Set<ServerEndpointConfig> getEndpointConfigs(Set<Class<? extends Endpoint>> endpointClasses) {
-        ServerEndpointConfig serverEndpointConfig = ServerEndpointConfig.Builder.create(WebSocketEndpoint.class, "/echo").build();
         Set<ServerEndpointConfig> configs = new HashSet<ServerEndpointConfig>();        
-        configs.add(serverEndpointConfig);
         
+        ServerEndpointConfig serverEndpointConfig = ServerEndpointConfig.Builder.create(WebSocketEndpoint.class, "/echo").build();
         ServerEndpointConfig anotherServerEndpointConfig = ServerEndpointConfig.Builder.create(AnotherWebSocketEndpoint.class, "/another").build();
+        
+        configs.add(serverEndpointConfig);
         configs.add(anotherServerEndpointConfig);
+        
         return configs;
     }
 
